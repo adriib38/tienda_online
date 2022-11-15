da40766411a716bf027340f2f820d3f3306a270a
Adrián <39964431+adriib38@users.noreply.github.com>
2022-11-15 22:50:30 +0100

    Delete merchashop directory

--- merchashop/inc/cabecera.inc.php
+++
@@ -1,8 +0,0 @@
-<div id="cabecera">  
-    <img src="banner-img.png" width="220px">
-    <h1>MerchaShop AdriánBenítez</h2>
-</div>
-    <ul class="navbar">
-    <li><a href="index.php">Princial</a></li>
-    <li><a href="carrito.php"><i class="fa-solid fa-cart-shopping"><?=count($_SESSION)?? ''?></i></a><li>
-</ul>
