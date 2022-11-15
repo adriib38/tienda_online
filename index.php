8c3e5d40af7648fa0c59a7b3bcd2d6b00a0c494b
Adrián <39964431+adriib38@users.noreply.github.com>
2022-11-15 18:01:21 +0100

    15/11/2022

--- index.php
+++ index.php
@@ -1,9 +1,43 @@
 <?php
+    session_start();
 
+    print_r($_SESSION);
+
+    if(!empty($_GET)){
+
+        $cod = 'cod_'.$_GET['product'].'';
+
+        if($_GET['action'] == 'sumar'){
+            if(isset($_SESSION[$cod])){
+                $_SESSION[$cod]++;
+            }else{
+                $_SESSION[$cod] = 1;
+            }
+        }
+
+        if($_GET['action'] == 'restar'){
+            if(isset($_SESSION[$cod])){
+                if($_SESSION[$cod] > 0){
+                    $_SESSION[$cod] = $_SESSION[$cod] - 1;
+                }else{
+                    $_SESSION[$cod] == 0;
+                    unset($_SESSION[$cod]);
+                }
+            }
+        }
+
+        if($_GET['action'] == 'eliminar'){
+            if(isset($_SESSION[$cod])){
+                unset($_SESSION[$cod]);
+            }
+        }
+
+        header('Location: index.php');
+    }
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,8 +45,11 @@
     <title>Inicion</title>
 
     <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/92a45f44ad.js" crossorigin="anonymous"></script>
 </head>
 <body>
+
+    <?php include('inc/cabecera.inc.php') ?>
     <h2>Productos</h2>
     
     <?php
@@ -31,6 +68,18 @@
                 <p><?=$registro['precio']??'' ?></p>
                 <img width="250px" src="<?=$registro['imagen']??'' ?>">
                 <p>Stock <?=$registro['stock']??'' ?></p>
+                <div class="product-buttons">
+                    <a href="index.php?action=sumar&product=<?=$registro['codigo']??'' ?>">
+                        <i class="fa-solid fa-circle-plus"></i>
+                    </a>
+                    <a href="index.php?action=restar&product=<?=$registro['codigo']??'' ?>">
+                        <i class="fa-solid fa-circle-minus"></i>
+                    </a>
+                    <a href="index.php?action=eliminar&product=<?=$registro['codigo']??'' ?>">
+                        <i class="fa-solid fa-trash-can"></i>
+                    </a>
+
+                </div>
             </div>
         <?php
         }
